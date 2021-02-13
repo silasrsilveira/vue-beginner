c316aefdfeeed36fc9fb50ce9bcd8d5d2d7ac80f
Silas Ribeiro <silass@gmail.com>
2021-02-13 11:05:55 -0400

    Aplicando o DATA Binding one and two way

---
+++ src/components/Cliente.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="cliente">
+
+    <input type="text" :model="nome">
+    <hr>
+    <input type="text" v-model="descricao">
+
+    
+    <h2 id="cliente-nome">{{nome}}, {{numero}}</h2>
+    <h3>{{descricao}}</h3>
+   
+
+    </div>
+   
+    
+</template>
+
+
+<script>
+export default {
+    data(){
+        return {
+            nome: "Silas Ribeiro da Silveira",
+            numero: "992179862",
+            email: "",
+            idade: 0, 
+            descricao: "Lorem ipsum Lorem Ipsum"
+        }
+    }
+}
+
+
+</script>
+
+
+<style scoped>
+    #cliente{
+     
+    }
+    #cliente-nome{
+            }
+    
+
+</style>
---
+++ src/components/Produto.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="janela-prod">
+      
+      <h3 id="produto">Meu produto</h3>
+      <p> Produto de linha</p>
+      <input type="text" placeholder="Preço">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    #produto{
+        color: indigo;
+        background: greenyellow;
+        max-width: 300px;
+    }
+    #janela-prod{
+        }
+</style>
