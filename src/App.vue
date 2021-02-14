168ae7dc60a1d950d640ccbed100b3ba077767e5
Silas Ribeiro <silass@gmail.com>
2021-02-14 11:57:48 -0400

    Adicionado um formulario

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
+
+                <h3>Cadastro de Clientes</h3>
+
+                <input type="text" placeholder="nome" v-model="nomeField"><br>
+                <input type="text" placeholder="email" v-model="emailField"><br>
+                <input type="text" placeholder="idade" v-model="idadeField"><br>
+                <button @click="cadastrarUsuarios">Cadastrar</button>
+
         <div v-for="(cliente,index) in clientes" :key="cliente.id">
           <h4>{{index + 1}}</h4>
             <Cliente :cliente="cliente"/>
             <hr>
             <h4>Edição: </h4>                  
-            <input type="text" v-model="cliente.nome">
-            <input type="text" v-model="cliente.email">
+
         </div>
-
-
 
      </div>
 </template>
@@ -18,63 +23,42 @@
 import Cliente from './components/Cliente';
 //import Produto from './components/Produto';
 export default {
-  name: 'App',
-  data(){
-    return{
-      nomeDoSilas: "Silas R. Silveira",
-      clienteSilas: {
-        nome: "Silas Ribeiro",
-        email: "silas@email.com.br" ,
-        idade: 25,
-        
-      },
-      clientes: [
-        { 
-          id: 1,            
+    name: 'App',
+    data(){
+      return{
+
+        nomeField: "",
+        emailField: "",
+        idadeField: "",
+        clientes: [
+      { 
+              id: 1,            
               nome: "Silas Ribeiro",
               email: "silas@email.com.br" ,
               idade: 25
       },
-    {
+      {
       id: 2,
               nome: "Paulo Junior",
               email: "paulo@email.com.br" ,
               idade: 24
-
-      },
-        
-    { 
-      id: 3,
-              nome: "Isaias Santos",
-              email: "isaias@email.com.br" ,
-              idade: 26
-
-          },
-    { 
-      id: 4,
-              nome: "Eunice Costa",
-              email: "nice@email.com.br" ,
-              idade: 51
-
-          },
-    { 
-      id: 5,
-              nome: "Lidiane Ribeiro",
-              email: "ldi@email.com.br" ,
-              idade: 18
-
-          },
-        
-        ]
-      }
+       } 
+     ]
+    }
   },
+          components: {
+      Cliente,
 
-
-  components: {
-    Cliente,
-    //Produto
-  }
-}
+  },
+          methods: {
+          cadastrarUsuarios: function(){
+    this.clientes.push({nome: this.nomeField, email: this.emailField, idade: this.emailField, id: Date.now()})
+    this.nomeField = "",
+    this.emailField = "",
+    this.idadeField = 0
+       }
+     }
+  } 
 
 </script>
     
